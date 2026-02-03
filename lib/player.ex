548a699010326ebc99f50670324a9333339f52ec
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-03 09:59:17 -0300

    add build player function

--- lib/player.ex
+++ lib/player.ex
@@ -1,4 +1,22 @@
 defmodule ElixirExMonGame.Player do
   @enforce_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
   defstruct [:life, :name, :move_rnd, :move_avg, :move_heal]
+
+  # ElixirExMonGame.Player.build("Lucas", :chute, :soco, :cura)
+  @spec build(any(), any(), any(), any()) :: %ElixirExMonGame.Player{
+          life: 100,
+          move_avg: any(),
+          move_heal: any(),
+          move_rnd: any(),
+          name: any()
+        }
+  def build(name, move_rnd, move_avg, move_heal) do
+    %ElixirExMonGame.Player{
+      life: 100,
+      name: name,
+      move_rnd: move_rnd,
+      move_avg: move_avg,
+      move_heal: move_heal
+    }
+  end
 end
