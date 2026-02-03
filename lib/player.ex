e66f98aeeb61e0c275edfb90f50e56221a36e6ea
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-03 10:08:30 -0300

    lint

--- lib/player.ex
+++ lib/player.ex
@@ -1,11 +1,14 @@
 defmodule ElixirExMonGame.Player do
-  @required_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
+  @required_keys [:life, :move_avg, :move_heal, :move_rnd, :name]
+  @max_life 100
+
   @enforce_keys @required_keys
   defstruct @required_keys
 
-  @max_life 100
-
-  # ElixirExMonGame.Player.build("Lucas", :chute, :soco, :cura)
+  @doc """
+    Builds a player struct
+    ElixirExMonGame.Player.build("Lucas", :chute, :soco, :cura)
+  """
   @spec build(any(), any(), any(), any()) :: %ElixirExMonGame.Player{
           life: 100,
           move_avg: any(),
@@ -16,10 +19,10 @@ defmodule ElixirExMonGame.Player do
   def build(name, move_rnd, move_avg, move_heal) do
     %ElixirExMonGame.Player{
       life: @max_life,
-      name: name,
-      move_rnd: move_rnd,
       move_avg: move_avg,
-      move_heal: move_heal
+      move_heal: move_heal,
+      move_rnd: move_rnd,
+      name: name
     }
   end
 end
