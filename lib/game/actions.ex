4b7a988127de27e29838e658251fbdcf30cb3aad
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-03 15:17:39 -0300

    add attack and heal method to action module

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,5 +1,21 @@
 defmodule ElixirExMonGame.Game.Actions do
   alias ElixirExMonGame.Game
+  alias ElixirExMonGame.Game.Actions.{Attack, Heal}
+
+  def attack(move) do
+    case Game.turn() do
+      :player -> Attack.attack_opponent(:computer, move)
+      :computer -> Attack.attack_opponent(:player, move)
+    end
+  end
+
+  def heal do
+    case Game.turn() do
+      :player -> Heal.heal_player(:player)
+      :computer -> Heal.heal_player(:computer)
+    end
+  end
+
   def fetch_move(move) do
     Game.player()
     |> Map.get(:moves)
