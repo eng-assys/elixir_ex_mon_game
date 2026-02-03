21175ce1feec90f205f3d3acb75ab76c43b6ce2b
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-03 15:21:55 -0300

    add attack module

---
+++ lib/game/actions/attack.ex
@@ -0,0 +1,12 @@
+defmodule ElixirExMonGame.Game.Actions.Attack do
+  @move_avg_power 18..25
+  @move_rnd_power 10..35
+
+  def attack_opponent(opponent, move) do
+    IO.puts("#{opponent} is being attacked using #{move}!")
+    calculate_power(move)
+  end
+
+  defp calculate_power(:move_avg), do: Enum.random(@move_avg_power)
+  defp calculate_power(:move_rnd), do: Enum.random(@move_rnd_power)
+end
